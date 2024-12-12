f5d80c74932ca4dd8cdec1f7af6a7cc786352522
Sufian <sufianbhatti76@gmail.com>
2024-12-12 15:14:30 -0500

    updated schema

--- models/Entertainment.js
+++ models/Entertainment.js
@@ -5,13 +5,17 @@ const EntertainmentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   publicationDate: { type: Date, required: true },
   genre: { type: [String], required: true },
-  type: { 
-    type: String, 
-    enum: ["book", "movie", "show", "game"], 
-    required: true 
+  type: {
+    type: String,
+    enum: ["book", "movie", "show", "game"],
+    required: true
   },
   details: { type: Object, required: true },
-  rating: { type: Number, min: 0, max: 10 }
+  rating: { type: Number, min: 0, max: 10 },
+  img_url: {
+    type: String,
+    required: false,
+  },
 });
 
 // Export the model
